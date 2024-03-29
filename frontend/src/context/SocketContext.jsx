18a12b5d5b46850e18f10e7290b9aede16976ad9
Markobanduka <banduka44@gmail.com>
2024-03-29 23:38:32 +0100

    Updated link

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://chat-app.onrender.com", {
+      const socket = io("https://chat-app-na7c.onrender.com", {
         query: {
           userId: authUser._id,
         },
